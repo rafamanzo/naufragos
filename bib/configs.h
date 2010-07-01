661991018eccf64618524fa3a560f2e7a6043086
Rafael Reggiani Manzo <rafamz@linux.ime.usp.br>
2010-07-01 15:52:44 -0300

    Bison e flex quase funcionando

---
+++ bib/configs.h
@@ -0,0 +1,13 @@
+typedef struct mnt{
+  int comprimento;
+  int altura;
+} monitor;
+
+static int jogadores;
+static int vidas_iniciais;
+static monitor tela;
+int velocidade_maxima;
+static int velocidade_minima;
+static int frequencia_criacao_pessoas;
+static int velocidade_criacao_pessoas;
+static int numero_recifes;
