5ea3365e8c4c6d82b10859351d468fb89ee10dbe
Rafael Reggiani Manzo <rafamz_lol@yahoo.com.br>
2010-07-03 14:18:25 -0300

    Entidade botes feita (funcoes adaptadas a nova ED e colocadas em um arquivo separador

--- entidades/botes.c
+++ entidades/botes.c
@@ -3,6 +3,7 @@
 #include "../bib/tipos.h"
 #include "../bib/mar.h"
 #include "../configurador/configs.h"
+#include "botes.h"
 
 lista_botes geraBotes(lista_pessoas lista_p, lista_estaticos lista_e, lista_botes lista_b)
 {
@@ -13,9 +14,8 @@ lista_botes geraBotes(lista_pessoas lista_p, lista_estaticos lista_e, lista_bote
 	b1.jogador = 1;
 	b1.vidas = vidas_max;
 	b1.carga = 0;
-/**
-  	boteBorda(naufragos, &b1,l_max,c_max);
-**/
+
+	boteBorda(lista_p, lista_e, lista_b, &b1);
 
 	lista_b = insereBote(lista_b, b1);
 
@@ -24,9 +24,8 @@ lista_botes geraBotes(lista_pessoas lista_p, lista_estaticos lista_e, lista_bote
 	b2.jogador = 2;
 	b2.vidas = vidas_max;
 	b2.carga = 0;
-/**
-  	boteBorda(naufragos, &b2,l_max,c_max);
-**/
+
+ 	boteBorda(lista_p, lista_e, lista_b, &b2);
 
 	lista_b = insereBote(lista_b, b2);
 
@@ -41,7 +40,7 @@ void boteBorda(lista_pessoas lista_p, lista_estaticos lista_e, lista_botes lista
 	bote->atr.vel.x = (rand()%(int)((velocidade_maxima + 1 - velocidade_minima))) + velocidade_minima;
 	bote->atr.vel.y = (rand()%(int)((velocidade_maxima + 1 - velocidade_minima))) + velocidade_minima;
 
-	while(1)
+	while(!validaPos(lista_p, lista_e, lista_b, NULL, NULL, bote))
 	{
 		novaBorda = rand()%4;
 		if(novaBorda == 0) /* Canto superior esquerdo */
@@ -64,9 +63,68 @@ void boteBorda(lista_pessoas lista_p, lista_estaticos lista_e, lista_botes lista
 			bote->atr.pos.x = bote->atr.raio;
 			bote->atr.pos.y = tela.altura - bote->atr.raio;
 		}
-
-		if(validaPos(lista_p, lista_e, lista_b, NULL, NULL bote))
-			break;
 	}
 }
 
+void moveBote(lista_botes b, double deltaT)
+{
+	int dir;
+	double vel;
+
+	vel = modulo(b->atr.vel);
+  dir = direcao(b->atr.vel.x, b->atr.vel.y);
+
+  	switch(dir)
+	{
+    	case L:
+      		b->atr.pos.x += (int) vel*deltaT;
+      		break;
+    	case NE:
+      		b->atr.pos.x += (int) vel*cos(M_PI/4)*deltaT;
+      		b->atr.pos.y += (int) vel*sin(M_PI/4)*(-1)*deltaT;
+      		break;
+    	case N:
+      		b->atr.pos.y += (int) vel*(-1)*deltaT;
+      		break;
+    	case NO:
+      		b->atr.pos.x += (int) vel*cos(M_PI/4)*(-1)*deltaT;
+      		b->atr.pos.y += (int) vel*sin(M_PI/4)*(-1)*deltaT;
+      		break;
+    	case O:
+      		b->atr.pos.x += (int) vel*(-1)*deltaT;
+      		break;
+    	case SO:
+      		b->atr.pos.x += (int) vel*cos(M_PI/4)*(-1)*deltaT;
+      		b->atr.pos.y += (int) vel*sin(M_PI/4)*deltaT;
+     		break;
+    	case S:
+      		b->atr.pos.y += (int) vel*deltaT;
+      		break;
+    	case SE:
+      		b->atr.pos.x += (int) vel*cos(M_PI/4)*deltaT;
+      		b->atr.pos.y += (int) vel*sin(M_PI/4)*deltaT;
+      		break;
+  	}
+}
+
+
+void colideBotes(lista_botes b1, lista_botes b2, double deltaT)
+{
+/*Colisao perfeitamente elastica entre objetos de mesma massa, suas direcoes e velocidades se invertem.*/
+    
+	vetor aux_vel;
+
+	aux_vel = p1->atr.vel; 
+	b1->atr.vel = p2->atr.vel;
+	b2->atr.vel = aux_vel;
+
+	moveBote(b1,deltaT);
+	moveBote(b2,deltaT);
+
+	if(distancia(b1->atr.pos, b2->atr.pos) > (b1->atr.raio + b2->atr.raio))
+	{
+	  	b1->atr.atualizada = 1;
+	  	b2->atr.atualizada = 1;
+ 	}
+  
+}
