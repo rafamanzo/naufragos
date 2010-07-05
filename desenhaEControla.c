597b860a46db62c2be65c9c328d85736d2e1b918
Giancarlo Rigo <gian@gian-laptop.(none)>
2010-07-05 18:24:20 -0300

    circulos vermelhos

--- desenhaEControla.c
+++ desenhaEControla.c
@@ -4,6 +4,9 @@
 #include "bib/desenhaEControla.h"
 #include "entidades/botes.h"
 
+#define ACELERACAO 10
+#define FREIO 20
+
 void imprimeMar(lista_pessoas lista_p, lista_estaticos lista_e, lista_botes lista_b, double deltaT)
 {
 	BITMAP *buffer, *fundo;
@@ -15,10 +18,10 @@ void imprimeMar(lista_pessoas lista_p, lista_estaticos lista_e, lista_botes list
 	blit(fundo, buffer,0,0,0,0, 1024, 768);	/* alterar o tamanho da imagem com o da tela */
 	
 	/* Imprime as pessoas */
-	/*imprimePessoas(buffer, lista_p);*/
+	imprimePessoas(buffer, lista_p);
 
 	/* Imprime os estaticos */
-	/*imprimeEstaticos(buffer, lista_e);*/
+	imprimeEstaticos(buffer, lista_e);
 
 	/* Imprime e controla os botes */
 	controlaBote1(buffer, &lista_b -> bt, deltaT);
@@ -61,13 +64,15 @@ void controlaBote1(BITMAP *buffer, bote *boat, double deltaT)
 		else if (key[KEY_UP])/*propulsao*/
 		{
 			ang = rotacaoBote(dir);
-			moveBote(dir, 10,boat, deltaT);
+			moveBote(dir, ACELERACAO,boat, deltaT);
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 		}
 		else if (key[KEY_DOWN])/*freio*/
 		{
-			if( (modulo(boat->atr.vel)-20) > 0 )
-			moveBote(dir,(-20),boat, deltaT);
+			if( (modulo(boat->atr.vel)-FREIO) > 0 )
+				moveBote(dir,((-1)*FREIO),boat, deltaT);
+			else
+				moveBote(dir,(-1)*modulo(boat->atr.vel),boat, deltaT);
 
 			ang = rotacaoBote(dir);
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);
@@ -75,7 +80,8 @@ void controlaBote1(BITMAP *buffer, bote *boat, double deltaT)
 		else if (key[KEY_ENTER])
 		{
 			ang = rotacaoBote(dir);
-			boat->ancora = '1';				
+			boat->ancora = '1';
+			moveBote(dir,(-1)*modulo(boat->atr.vel),boat, deltaT);				
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 		}
 		else  
@@ -87,12 +93,13 @@ void controlaBote1(BITMAP *buffer, bote *boat, double deltaT)
 	}
 	else
 	{
-		if (key[KEY_ENTER]) /* Nao volta com velocidade 0 */
+		if (key[KEY_ENTER])
 			boat->ancora = '0';
 		
 		ang = rotacaoBote(dir);
 		rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 	}
+	circle(buffer, boat->atr.pos.x,boat->atr.pos.y ,boat->atr.raio,makecol(255, 0, 0));
 }
 
 void controlaBote2(BITMAP *buffer, bote *boat, double deltaT)
@@ -123,13 +130,15 @@ void controlaBote2(BITMAP *buffer, bote *boat, double deltaT)
 		else if (key[KEY_W])/*propulsao*/
 		{
 			ang = rotacaoBote(dir);
-			moveBote(dir, 10,boat,deltaT);
+			moveBote(dir,ACELERACAO,boat,deltaT);
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 		}
 		else if (key[KEY_S])/*freio*/
 		{
-			if( (modulo(boat->atr.vel)-20) > 0 )
-					moveBote(dir,(-20),boat,deltaT);
+			if( (modulo(boat->atr.vel)-FREIO) > 0 )
+					moveBote(dir,((-1)*FREIO),boat,deltaT);
+			else
+				moveBote(dir,(-1)*modulo(boat->atr.vel),boat, deltaT);
 
 			ang = rotacaoBote(dir);
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);
@@ -137,7 +146,8 @@ void controlaBote2(BITMAP *buffer, bote *boat, double deltaT)
 		else if (key[KEY_SPACE])
 		{
 			ang = rotacaoBote(dir);
-			boat->ancora = '1';				
+			boat->ancora = '1';
+			moveBote(dir,(-1)*modulo(boat->atr.vel),boat, deltaT);				
 			rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 		}
 		else  
@@ -149,12 +159,13 @@ void controlaBote2(BITMAP *buffer, bote *boat, double deltaT)
 	}
 	else
 	{
-		if (key[KEY_SPACE]) /* Nao volta com velocidade 0 */
+		if (key[KEY_SPACE]) 
 			boat->ancora = '0';
 		
 		ang = rotacaoBote(dir);
 		rotate_sprite(buffer, boat->atr.desenho, boat->atr.pos.x, boat->atr.pos.y, ang);	
 	}
+	circle(buffer, boat->atr.pos.x,boat->atr.pos.y ,boat->atr.raio,makecol(255, 0, 0));
 }
 
 void imprimePessoas(BITMAP *buffer, lista_pessoas pessoas)
@@ -165,6 +176,7 @@ void imprimePessoas(BITMAP *buffer, lista_pessoas pessoas)
 	while( aux != NULL)
 	{
 		draw_sprite(buffer, aux -> pss.atr.desenho, aux -> pss.atr.pos.x, aux -> pss.atr.pos.y);
+		circle(buffer, aux->pss.atr.pos.x,aux->pss.atr.pos.y ,aux->pss.atr.raio,makecol(255, 0, 0));
 		aux = aux -> prox;
 	}
 
@@ -178,11 +190,13 @@ void imprimeEstaticos(BITMAP *buffer, lista_estaticos estat)
 	while( aux != NULL)
 	{
 		if( aux -> stc.tipo == 'r' )
-			 blit(aux->stc.desenho, buffer, 0, 0, 0, 0, 25, 30);/*TAMANHO DO CORAL */
+			draw_sprite(buffer, aux->stc.desenho, aux->stc.pos.x, aux->stc.pos.y);
 		else
-			draw_sprite(buffer, aux -> stc.desenho, aux -> stc.pos.x, aux -> stc.pos.y);
+			draw_sprite(buffer, aux->stc.desenho, aux->stc.pos.x-132, aux->stc.pos.y-66);
 
+	 circle(buffer, aux->stc.pos.x,aux->stc.pos.y ,aux->stc.raio,makecol(255, 0, 0));
 	 aux = aux -> prox;
+		
 	}
 }
 
@@ -201,25 +215,25 @@ void imprimeCabecalho(BITMAP *buffer, lista_botes botes)
 	/* VIDAS */
 	coracao = load_bitmap("imagens/coracao.bmp",desktop_palette);
 	
-	textprintf_ex(buffer, font, 120, 20, makecol(0, 0, 255), -1, "%u x", botes -> bt.vidas);
+	textprintf_ex(buffer, font, 120, 20, makecol(0, 0, 255), -1, "%u x", botes -> prox -> bt.vidas);
 	draw_sprite(buffer, coracao, 150, 10);
 	
-	textprintf_ex(buffer, font, 780, 20, makecol(255, 0, 0), -1, "%u x", botes -> prox -> bt.vidas);
+	textprintf_ex(buffer, font, 780, 20, makecol(255, 0, 0), -1, "%u x", botes ->  bt.vidas);
 	draw_sprite(buffer, coracao, 810, 10);
 
 	/* CARGA */
 	pessoa = load_bitmap("imagens/pessoa2.bmp",desktop_palette);
 	
-	textprintf_ex(buffer, font, 190, 20, makecol(0, 0, 255), -1, "%u x", botes -> bt.carga);
+	textprintf_ex(buffer, font, 190, 20, makecol(0, 0, 255), -1, "%u x", botes -> prox -> bt.carga);
 	draw_sprite(buffer, pessoa, 220, 15);
 	
-	textprintf_ex(buffer, font, 850, 20, makecol(255, 0, 0), -1, "%u x", botes -> prox -> bt.carga);
+	textprintf_ex(buffer, font, 850, 20, makecol(255, 0, 0), -1, "%u x", botes -> bt.carga);
 	draw_sprite(buffer, pessoa, 880, 15);
 
 
 	/* PONTOS */
-	textprintf_ex(buffer, font, 260, 20, makecol(0, 0, 255), -1, "%d", botes -> bt.pontos);
-	textprintf_ex(buffer, font, 720, 20, makecol(255, 0, 0), -1, "%d", botes -> prox -> bt.pontos);
+	textprintf_ex(buffer, font, 260, 20, makecol(0, 0, 255), -1, "%d", botes -> prox -> bt.pontos);
+	textprintf_ex(buffer, font, 720, 20, makecol(255, 0, 0), -1, "%d", botes -> bt.pontos);
 
 	/* ANCORAS */
 	if( botes -> prox -> bt.ancora == '1')
